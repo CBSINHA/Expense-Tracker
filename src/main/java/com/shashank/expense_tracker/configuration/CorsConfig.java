19004ed8b63be476e86e1939af525dfaf5322f39
Shashank Sinha <shashanksinha590@gmail.com>
2026-05-19 19:08:36 +0530

    Added vercel link in cors config

--- src/main/java/com/shashank/expense_tracker/configuration/CorsConfig.java
+++ src/main/java/com/shashank/expense_tracker/configuration/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
 
                         .allowedOrigins(
-                                "http://localhost:5173"
+                                "http://localhost:5173", "https://expense-tracker-frontend-brown-xi.vercel.app/"
                         )
 
                         .allowedMethods("*")
